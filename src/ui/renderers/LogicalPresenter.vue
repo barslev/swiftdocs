06ed3be60d10ebb5a877a5579ae590fb3b5eb9f2
Ahmet Özışık <ozisikahmet@gmail.com>
2019-04-24 14:17:11 +0300

    perf: move logical presenter's work into renderTree

--- src/ui/renderers/LogicalPresenter.vue
+++ src/ui/renderers/LogicalPresenter.vue
@@ -1,9 +1,8 @@
 <template>
     <div :is="htmlTag">
-        <el v-for="(item, index) in displayItems"
+        <el v-for="(item, index) in items"
             :key="'item-' + index + '-' + item.id"
-            :element="item"
-            :context="item.context">
+            :element="item">
         </el>
     </div>
 </template>
@@ -11,146 +10,10 @@
 import {connect} from '~/redux/connect'
 import {getContentState} from '~/redux/actions/contents'
 
-function comparable(value) {
-    if(_.isArray(value)) {
-        return value.length
-    }
-    if(_.isNumber(value) || _.isString(value)) {
-        return parseFloat(value)
-    }
-    if(_.isObject(value)) {
-        return Object.keys(value)
-    }
-    return value
-}
-
 export default {
     props: [
         'items',
-        'context',
         'htmlTag'
-    ],
-
-    mixins: [
-        connect((state, scope) => {
-            return {
-                inRenderMode: state.session.mode
-            }
-        })
-    ],
-
-    data() {
-        return {
-            displayItems: [],
-        }
-    },
-
-    watch: {
-        items() {
-            this.refresh()
-        },
-        'state.inRenderMode'() {
-            this.refresh()
-        }
-    },
-
-    methods: {
-        refresh() {
-            if (this.state.inRenderMode) {
-                // Display a rendered version of items
-                this.displayItems = this.displayRenderedItems(
-                    // Pass a clone of the current items
-                    [].concat(this.items)
-                )
-                return
-            }
-            // Display them as is
-            this.displayItems = this.items
-        },
-
-        displayRenderedItems(items) {
-            items = this.applyLoops(items)
-            items = this.applyConditions(items)
-            return Vue.nonreactive(items)
-        },
-
-        applyLoops(items) {
-            let result = []
-            _.each(items, (item) => {
-                let state = getContentState(item.id)
-                let loop = _.get(state, 'logic.loop')
-                if (loop) {
-                    result = result.concat(this.getLoopedItems(item, loop))
-                } else {
-                    item.context = Vue.nonreactive(this.context)
-                    result.push(item)
-                }
-            })
-            return result
-        },
-
-        getLoopedItems(item, loop) {
-            let items = []
-            const indexAs = loop.index_as ? loop.index_as : '$index'
-
-            _.each(_.get(this.context, loop.in), (foo, $index) => {
-
-                items.push({
-                    ...item,
-                    context: Vue.nonreactive({
-                        ...this.context,
-                        [indexAs]: $index,
-                        [loop.as]: foo,
-                    })
-                })
-            })
-            return items
-        },
-
-        applyConditions(items) {        
-            return _.filter(items, (item) => {
-                let state = getContentState(item.id)
-                let condition = _.get(state, 'logic.conditions')
-                if (!condition) {
-                    return true
-                }
-                return _.get(state, 'logic.conditions', []).every(condition => {
-                    return this.evaluateCondition(condition)
-                })
-            })
-        },
-
-        evaluateCondition(condition) {
-            try {
-                const value = _.get(
-                    this.context,
-                    condition.address
-                )
-
-                switch (condition.comparator) {
-                    case 'exists':
-                        return typeof value !== 'undefined' && value !== null
-                    case 'truthy':
-                        return value
-                    case 'falsy':
-                        return !value
-                    case 'equals':
-                        return value == condition.value
-                    case 'not_equal':
-                        return value != condition.value                        
-                    case 'greater_than':
-                        return comparable(value) > parseFloat(condition.value)
-                    case 'less_than':
-                        return comparable(value) < parseFloat(condition.value)
-                    default:
-                        throw new Error('Unknown comparator: ' + condition.comparator)
-                }
-
-            } catch(e) {
-                console.error(e)
-                return false
-            }
-        }
-    },
+    ]
 }
 </script>
---
+++ src/ui/renderers/renderTree.js
@@ -0,0 +1,111 @@
+import Immutable from 'seamless-immutable';
+import {getContextData} from '~/redux/actions/data'
+import {getContentState} from '~/redux/actions/contents'
+
+const comparable = value => {
+    if (_.isArray(value)) {
+        return value.length
+    } else if (_.isNumber(value) || _.isString(value)) {
+        return parseFloat(value)
+    } else if (_.isObject(value)) {
+        return Object.keys(value)
+    }
+    return value
+}
+
+// Second parameter is data, but it's not used.
+export default (contents, data, inRenderMode) => {
+    // Create immutable data
+    data = Immutable(data);
+    contents = Immutable(contents);
+
+   const applyLogic = (contents) => {
+        if (!inRenderMode) {
+            return contents
+        }
+        contents = applyLoops(contents)
+        contents = applyConditions(contents)
+        // Add more side effects...
+        return contents
+    }
+
+    const applyLoops = contents => {
+        return contents.reduce((carry, content) => {
+            let loop = _.get(getContentState(content.id), 'logic.loop')
+            if (!loop) {
+                carry.push(content)
+                return carry
+            }
+            return carry.concat(loopContent(content, loop))
+        }, [])
+    }
+
+    const loopContent = (content, loop) => {
+        const indexAs = loop.index_as ? loop.index_as : '$index'
+        const array = getContextData(loop.in, content.context, [])
+
+        return _.map(array, (arrayItem, $index) => content.merge({
+            context: {
+                ...content.context,
+                [indexAs]: $index,
+                [loop.as]: arrayItem
+            }
+        }))
+    }
+
+    const applyConditions = (contents) => {
+        return _.filter(contents, (content) => {
+            let state = getContentState(content.id)
+            let condition = _.get(state, 'logic.conditions')
+            if (!condition) {
+                return true
+            }
+            return _.get(state, 'logic.conditions', []).every(condition => {
+                return evaluateCondition(content, condition)
+            })
+        })
+    }
+
+    const evaluateCondition = (content, condition) => {
+        try {
+            const value = getContextData(
+                condition.address,
+                content.context
+            )
+            switch (condition.comparator) {
+                case 'exists':
+                    return typeof value !== 'undefined' && value !== null
+                case 'truthy':
+                    return value
+                case 'falsy':
+                    return !value
+                case 'equals':
+                    return value == condition.value
+                case 'not_equal':
+                    return value != condition.value                        
+                case 'greater_than':
+                    return comparable(value) > parseFloat(condition.value)
+                case 'less_than':
+                    return comparable(value) < parseFloat(condition.value)
+                default:
+                    throw new Error('Unknown comparator: ' + condition.comparator)
+            }
+        } catch(e) {
+            console.error(e)
+            return false
+        }
+    }    
+    
+    const contentsWithChildren = (containerId = null, context = {}) => {
+        let children = contents.filter(c => c.container_id === containerId)
+        if (inRenderMode) {
+            children = applyLogic(children.map(child => child.merge({context})))
+        }
+        return children.map(child => child.merge({
+            children: contentsWithChildren(child.id, child.context)
+        }))
+    }
+   
+    // Null puts top level items at the top of the tree
+    return contentsWithChildren(null, data)
+}
